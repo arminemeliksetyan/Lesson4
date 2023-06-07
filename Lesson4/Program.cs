fd70ed1f3f31373f017e0214942f6097c2e77420
Cute <Cute@DESKTOP-QCOA7R0>
2023-06-07 19:10:05 +0400

    Add project files.

---
+++ Lesson4/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Lesson4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //long sum =1;
+            //for (int k = 1; k<30; k++) {
+            //    sum *= k;
+            //}
+            //Console.WriteLine(sum);
+
+            //long sum = 1;
+            //int k = 1;
+            //while (k < 5)
+            //{
+            //    sum *= k;
+            //}
+            //Console.WriteLine(sum);
+
+            //int sum = 0;
+            //for(int i=0; i<100; i++)
+            //{
+            //    if(i%3 == 0)
+            //    {
+            //        sum+= i;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+
+            //int[] array1 = new int[5];
+            //int[] array2 = new int[] {1,2,6,5,4};
+            //int sum = 0;
+            //int[] array3 = { 1, 5, 4, 7, 8, 5, 5 ,2,4,7};
+            //for(int z = 0; z<array3.Length; z++)
+            //{
+            //        if(z%2 == 0)
+            //    {
+            //        sum += array3[z];
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+        }
+    }
+}
